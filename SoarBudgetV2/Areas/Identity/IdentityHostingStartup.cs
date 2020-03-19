54f92735dd8a12fbdf0966a5d4e18c21ea5864f7
Zachary Melton <zac.melton@outlook.com>
2020-03-19 15:22:34 -0500

    Scaffolded identity. Now will have the ability to register and save users

---
+++ SoarBudgetV2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SoarBudgetV2.Data;
+
+[assembly: HostingStartup(typeof(SoarBudgetV2.Areas.Identity.IdentityHostingStartup))]
+namespace SoarBudgetV2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
